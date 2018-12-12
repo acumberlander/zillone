bd514e8ae45e1f9f33779643a94f1b2c9859ce1b
Austin Cumberlander <austincumberlander@gmail.com>
2018-12-11 19:34:27 -0600

    wip

--- src/App/App.js
+++ src/App/App.js
@@ -1,38 +1,18 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
-import logo from './logo.svg';
+import connection from '../Helpers/Data/connection';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Auth from '../components/Auth/auth';
 
 class App extends Component {
+  componentDidMount() {
+    connection();
+  }
+
   render() {
-    console.log(this);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <button className='btn btn-danger'>HELP ME</button>
-          <Button
-    tag="a"
-    color="success"
-    size="large"
-    href="http://reactstrap.github.io"
-    target="_blank"
->
-    View Reactstrap Docs
-</Button>
-        </header>
+        <Auth />
       </div>
     );
   }
---
+++ src/components/Auth/auth.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './auth.scss';
+
+class auth extends React.Component {
+  render() {
+    return(
+      <div className="Auth">
+        <button className="btn btn-danger">Login</button>
+      </div>
+    );
+  }
+}
+
+export default auth;
