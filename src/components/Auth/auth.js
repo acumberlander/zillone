90bc6ed4a2ef7a1252ab7cc811ef5d38d9a4a145
Austin Cumberlander <austincumberlander@gmail.com>
2018-12-11 20:14:25 -0600

    setup authentication

--- src/components/Auth/auth.js
+++ src/components/Auth/auth.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './auth.scss';
+import authRequests from '../../Helpers/Data/authRequests';
 
 class auth extends React.Component {
+  authenticateUser = (e) => {
+    e.preventDefault();
+    authRequests.authenticate()
+      .then(() => {
+      // do something
+      })
+      .catch(error => console.error('there was an error with auth', error));
+  }
+
   render() {
-    return(
+    return (
       <div className="Auth">
-        <button className="btn btn-danger">Login</button>
+        <button className="btn btn-danger" onClick={this.authenticateUser}>Login</button>
       </div>
     );
   }
